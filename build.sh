e4e2c863715f59817c16a0ef8c36a1c5e4648c5f
Alexandr <digital-mag@ya.ru>
2021-02-25 16:23:54 +0300

    deploy help

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 docker build -t web:latest .
 # sudo docker build -t registry.heroku.com/blooming-hollows-52049/web .
+# sudo docker push registry.heroku.com/blooming-hollows-52049/web
+# sudo heroku container:release -a blooming-hollows-52049 web
